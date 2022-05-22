9fe5924403dcaf62a2f6bc952166b2ed8cd8f0d7
jsfm888 <qinwenqiang_8@163.com>
2022-05-22 16:45:57 +0800

    third commit

--- src/main/java/com/zoom/git/GitTest.java
+++ src/main/java/com/zoom/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("kobe ~ ");
         System.out.println("curry ~ ");
+        System.out.println("west ~ ");
     }
 }
