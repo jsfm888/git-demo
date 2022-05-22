aa186cbbc28a613359e9228f4869f2e110a83591
wincher <53160378+jsfm888@users.noreply.github.com>
2022-05-22 17:24:35 +0800

    pull request commit

--- src/main/java/com/zoom/git/GitTest.java
+++ src/main/java/com/zoom/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("durant ~ ");
         System.out.println("master update");
         System.out.println("hot-fix update");
+        System.out.println("pull request");
 
     }
 }
