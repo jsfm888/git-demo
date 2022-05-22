1f1ec674d0e01ed503119145dd4fb0a1a98892cf
jsfm888 <qinwenqiang_8@163.com>
2022-05-22 16:55:59 +0800

    hot-fix first commit

--- src/main/java/com/zoom/git/GitTest.java
+++ src/main/java/com/zoom/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("kobe ~ ");
         System.out.println("curry ~ ");
         System.out.println("west ~ ");
+        System.out.println("durant ~ ");
     }
 }
